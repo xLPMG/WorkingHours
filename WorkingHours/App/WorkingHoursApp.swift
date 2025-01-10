a79a323068df8a70c4441427cbfe18cb8328f957
Luca-Philipp Grumbach <xxlpmghdxx@gmail.com>
2025-01-10 23:07:12 +0100

    set up project

---
+++ WorkingHours/App/WorkingHoursApp.swift
@@ -0,0 +1,18 @@
+//
+//  WorkingHoursApp.swift
+//  WorkingHours
+//
+//  Created by Luca-Philipp Grumbach on 10.01.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WorkingHoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkingHours/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  WorkingHours
+//
+//  Created by Luca-Philipp Grumbach on 10.01.25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    ContentView()
+}
